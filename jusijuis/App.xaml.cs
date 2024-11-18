9039ee5b9494b11a1bea3618ee44cd3e8960bf49
laboratorio <laboratorio@UPOAULA10207>
2024-11-18 08:04:54 -0500

    Agregar archivos de proyecto.

---
+++ jusijuis/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace jusijuis
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ jusijuis/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace jusijuis
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
